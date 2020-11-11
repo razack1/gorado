6e015ffebb09349ee0dc3b80346f77c587c3fe98
Razack <boukorazack26@gmail.com>
2020-11-11 08:38:11 +0100

    initial commit

---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import { AiFillThunderbolt } from 'react-icons/ai';
+import { GiCrystalBars } from 'react-icons/gi';
+import { GiCutDiamond, GiRock } from 'react-icons/gi';
+import { GiFloatingCrystal } from 'react-icons/gi';
+import { IconContext } from 'react-icons/lib';
+import {
+  PricingSection,
+  PricingWrapper,
+  PricingHeading,
+  PricingContainer,
+  PricingCard,
+  PricingCardInfo,
+  PricingCardIcon,
+  PricingCardPlan,
+  PricingCardCost,
+  PricingCardLength,
+  PricingCardFeatures,
+  PricingCardFeature
+} from './Pricing.elements';
+
+function Pricing() {
+  return (
+    <IconContext.Provider value={{ color: '#a9b3c1', size: 64 }}>
+      <PricingSection>
+        <PricingWrapper>
+          <PricingHeading>Nos services</PricingHeading>
+          <PricingContainer>
+            <PricingCard to='/sign-up'>
+              <PricingCardInfo>
+                <PricingCardIcon>
+                  <GiRock />
+                </PricingCardIcon>
+                <PricingCardPlan>Pack de Départ</PricingCardPlan>
+                <PricingCardCost>10000Fcfa</PricingCardCost>
+                <PricingCardLength>Par Mois</PricingCardLength>
+                <PricingCardFeatures>
+                  <PricingCardFeature>50 Clients</PricingCardFeature>
+                  <PricingCardFeature>$10,000 Budget</PricingCardFeature>
+                  <PricingCardFeature>Retargeting analytics</PricingCardFeature>
+                </PricingCardFeatures>
+                <Button primary>Choose Plan</Button>
+              </PricingCardInfo>
+            </PricingCard>
+            <PricingCard to='/sign-up'>
+              <PricingCardInfo>
+                <PricingCardIcon>
+                  <GiCrystalBars />
+                </PricingCardIcon>
+                <PricingCardPlan>Gold Rush</PricingCardPlan>
+                <PricingCardCost>$299.99</PricingCardCost>
+                <PricingCardLength>per month</PricingCardLength>
+                <PricingCardFeatures>
+                  <PricingCardFeature>1000 New Users</PricingCardFeature>
+                  <PricingCardFeature>$50,000 Budget</PricingCardFeature>
+                  <PricingCardFeature>Lead Gen Analytics</PricingCardFeature>
+                </PricingCardFeatures>
+                <Button primary>Choose Plan</Button>
+              </PricingCardInfo>
+            </PricingCard>
+            <PricingCard to='/sign-up'>
+              <PricingCardInfo>
+                <PricingCardIcon>
+                  <GiCutDiamond />
+                </PricingCardIcon>
+                <PricingCardPlan>Diamond Kings</PricingCardPlan>
+                <PricingCardCost>29999Fcfa</PricingCardCost>
+                <PricingCardLength>per month</PricingCardLength>
+                <PricingCardFeatures>
+                  <PricingCardFeature>Livraison Illimité</PricingCardFeature>
+                  <PricingCardFeature>Budget Illimité</PricingCardFeature>
+                  <PricingCardFeature>24/7 Support</PricingCardFeature>
+                </PricingCardFeatures>
+                <Button primary>Choose Plan</Button>
+              </PricingCardInfo>
+            </PricingCard>
+          </PricingContainer>
+        </PricingWrapper>
+      </PricingSection>
+    </IconContext.Provider>
+  );
+}
+export default Pricing;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Agence de Course et Livraison',
+  headline: 'Leader de la Géneration Spécialiste de Course et  Livraison',
+  description:
+    'Nous aidons les entreprises à livrer leurs produits. Notre équipe est unique et dynamique dans la livraison de vos produits et course.',
+  buttonLabel: 'Contactez-nous!',
+  imgStart: '',
+  img: require('../../images/svg-1.svg'),
+  alt: 'Credit Card',
+  start: ''
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Investissez maintenant avec GORADO Agricole',
+  headline: "Une plateforme d'investissement agricole ",
+  description:
+    "Rejoignez notre équipe d'investissement pour mieux stimulee la production extensive et contribuer à sa croissance et etre actionnaire.",
+  buttonLabel: 'Apprendre Plus',
+  imgStart: '',
+  img: require('../../images/svg-2.svg'),
+  alt: 'Vault',
+  start: ''
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Myro Food',
+  headline:
+    'GORADO m"aide à accroitre mes revenus en assurant la livraison de mes produits à mes clients en temps records ',
+  description:
+    "Leur équipe est dynamique et proactif! Je crois qu'ils vont conquérir le marché national d'ici peu",
+  buttonLabel: 'Contactez les',
+  imgStart: 'start',
+  img: require('../../images/profile.jpg'),
+  alt: 'Vault',
+  start: 'true'
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Livraison Sécurisée',
+  headline: 'Tout vos colis sont livrés dans un temps records et en toute sécurité',
+  description:
+    'Ne  vous en faites plus en ce qui concerne les livraisons. Notre équipe spécialisé et très eveillé vous rends la vie facile.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: require('../../images/svg-3.svg'),
+  alt: 'Vault',
+  start: 'true'
+};
